161fc65e0ba22edcbc78799326a46966ab924dd7
subhagpo <soak@microsoft.com>
2016-01-14 17:36:23 -0800

    Added the basic test for Newton

--- tests/e2e/layout.tests.js
+++ tests/e2e/layout.tests.js
@@ -1,6 +1,11 @@
 describe('Define your Suite', function() {  
  
     it('Define your Spec', function() {
-      
+        var loginButton = browser.element(by.id("login"));
+        loginButton.click();
+        
+        //expect the greetings to contain specific text:
+        var greetings = browser.element(by.id("displayName"));
+        expect(greetings.getText()).toContain("Subhag");
     })
 });
--- tests/unit/services.tests.js
+++ tests/unit/services.tests.js
@@ -1,10 +1,16 @@
 describe('Unit Test Suite', function() {
-   
+    var Questions;
     beforeEach(module('starter.services'));
-    
-    it ("Unit Test Spec", inject (function () {
-    
+        
+    beforeEach(inject(function (_Questions_) {
+        Questions = _Questions_;
     }));
-    
 
+    it('Unit Test Spec', inject(function(Questions) {
+        expect(Questions).toBeDefined();
+    }));
+
+    it('has 3 questions', inject(function(Questions) {
+        expect(Questions.all().length).toEqual(3);
+    }));
 });

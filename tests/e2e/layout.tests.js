0a4256d08ac90a35ba4bf75606fc3bb3ca356a10
Subhag Oak <soak@microsoft.com>
2016-01-14 19:35:25 -0800

    Template clean up

--- tests/e2e/layout.tests.js
+++ tests/e2e/layout.tests.js
@@ -1,11 +1,7 @@
 describe('Define your Suite', function() {  
  
     it('Define your Spec', function() {
-        var loginButton = browser.element(by.id("login"));
-        loginButton.click();
-        
-        //expect the greetings to contain specific text:
-        var greetings = browser.element(by.id("displayName"));
-        expect(greetings.getText()).toContain("Subhag");
+        //var loginButton = element(by.id("login"));
+        //loginButton.click();
     })
 });
--- tests/unit/services.tests.js
+++ tests/unit/services.tests.js
@@ -1,16 +1,10 @@
 describe('Unit Test Suite', function() {
-    var Questions;
+
     beforeEach(module('starter.services'));
         
-    beforeEach(inject(function (_Questions_) {
-        Questions = _Questions_;
+    beforeEach(inject(function () {
     }));
 
-    it('Unit Test Spec', inject(function(Questions) {
-        expect(Questions).toBeDefined();
-    }));
-
-    it('has 3 questions', inject(function(Questions) {
-        expect(Questions.all().length).toEqual(3);
+    it('Unit Test Spec', inject(function() {
     }));
 });

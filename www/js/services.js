8c2624b260fb6bd94bdc29bd4def9072f8b3475c
Subhag Oak <soak@microsoft.com>
2016-01-21 12:20:51 -0800

    Add template service.

--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,29 @@
 angular.module('starter.services', [])
+
+.factory('Questions', function() {
+  // Some fake testing data
+  var questions = [
+     {
+        Id: 0,
+        $value: 'Rate this workshop?',
+        Rating: null
+     }, 
+     {
+        Id: 1,
+        $value: 'Rate the sessions?',
+        Rating: null
+     },
+     {
+        Id: 2,
+        $value: 'Rate your overall experience?',
+        Rating: 8
+     }
+  ];
  
+  return {
+    all: function() {
+      return questions;
+    }
+  };
+});
 
